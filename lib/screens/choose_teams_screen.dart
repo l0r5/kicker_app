5a625337d717052a7f8534b42db618dc27acbbc3
Lars <lars.braitinger@gmail.com>
2019-06-17 17:49:57 +0200

    align choose teams screen to be centered

--- lib/screens/choose_teams_screen.dart
+++ lib/screens/choose_teams_screen.dart
@@ -34,54 +34,81 @@ class _ChooseTeamsScreenState extends State<ChooseTeamsScreen> {
                 decoration: BoxDecoration(color: Colors.lightGreen),
                 child: Column(
                   children: <Widget>[
-                    Row(children: <Widget>[Text('User: ${user.email}')]),
-                    Row(
-                      children: <Widget>[
-                        ChoosePlayerButton(
-                          playerNumber: 1,
-                          availablePlayers: lobby.usersOnlineList,
-                          notifyParent: refresh,
-                        ),
-                        ChoosePlayerButton(
-                          playerNumber: 3,
-                          availablePlayers: lobby.usersOnlineList,
-                          notifyParent: refresh,
-                        )
-                      ],
+                    Center(
+                      child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: Text('User: ${user.email}')),
                     ),
-                    Row(
-                      children: <Widget>[
-                        ChoosePlayerButton(
-                          playerNumber: 2,
-                          availablePlayers: lobby.usersOnlineList,
-                          notifyParent: refresh,
-                        ),
-                        ChoosePlayerButton(
-                          playerNumber: 4,
-                          availablePlayers: lobby.usersOnlineList,
-                          notifyParent: refresh,
-                        ),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          ChoosePlayerButton(
+                            playerNumber: 1,
+                            availablePlayers: lobby.usersOnlineList,
+                            notifyParent: refresh,
+                          ),
+                          ChoosePlayerButton(
+                            playerNumber: 3,
+                            availablePlayers: lobby.usersOnlineList,
+                            notifyParent: refresh,
+                          )
+                        ],
+                      ),
                     ),
-                    Row(
-                      children: <Widget>[
-                        RaisedButton(
-                          onPressed: () {
-                            match.setTeams();
-                            Scaffold.of(context)
-                                .showSnackBar(SnackBar(content: Text('START')));
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => MatchScreen()));
-                          },
-                          child: Text('START'),
-                        ),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          ChoosePlayerButton(
+                            playerNumber: 2,
+                            availablePlayers: lobby.usersOnlineList,
+                            notifyParent: refresh,
+                          ),
+                          ChoosePlayerButton(
+                            playerNumber: 4,
+                            availablePlayers: lobby.usersOnlineList,
+                            notifyParent: refresh,
+                          ),
+                        ],
+                      ),
                     ),
-                    Row(
-                      children: <Widget>[
-                        Text(
-                            'Team 1: ${match.getPlayerNameByNumber(1)} - ${match.getPlayerNameByNumber(2)}'
-                            '\nTeam 2: ${match.getPlayerNameByNumber(3)} - ${match.getPlayerNameByNumber(4)}'),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          RaisedButton(
+                            onPressed: () {
+                              match.setTeams();
+                              Scaffold.of(context).showSnackBar(
+                                  SnackBar(content: Text('START')));
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => MatchScreen()));
+                            },
+                            child: Text('START'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                              'Team 1: ${match.getPlayerNameByNumber(1)} - ${match.getPlayerNameByNumber(2)}'
+                              '\nTeam 2: ${match.getPlayerNameByNumber(3)} - ${match.getPlayerNameByNumber(4)}'),
+                        ],
+                      ),
                     ),
                   ],
                 ))));
