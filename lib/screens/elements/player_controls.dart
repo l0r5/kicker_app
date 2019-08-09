de9fde1296e22bc292cac865cb98da28d6ea0958
Lars <lars.braitinger@gmail.com>
2019-08-09 20:42:50 +0200

    add design tweaking of soccer field by also style the player controls

--- lib/screens/elements/player_controls.dart
+++ lib/screens/elements/player_controls.dart
@@ -19,7 +19,6 @@ class PlayerControls extends StatefulWidget {
 class _PlayerControlsState extends State<PlayerControls> {
   final match = getIt.get<Match>();
 
-
   _addGoalToPlayer() {
     int playerGoals;
     int teamGoals;
@@ -132,50 +131,84 @@ class _PlayerControlsState extends State<PlayerControls> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Row(
-        children: <Widget>[
-          RaisedButton(
-            onPressed: () {
-              _addGoalToPlayer();
-              widget.notifyParent();
-              Scaffold.of(context).showSnackBar(SnackBar(
-                  content:
-                      Text('Goal! +1 for Player${widget.playerNumber}: ${widget.playerName}')));
-            },
-            child: Text('+'),
-          ),
-        ],
-      ),
-      Row(
-        children: <Widget>[
-          Text('${widget.playerName}'),
-        ],
-      ),
-      Row(
-        children: <Widget>[
-          RaisedButton(
-            onPressed: () {
-              _removeGoalFromPlayer();
-              widget.notifyParent();
-              Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text(
-                      'Remove one Goal from Player${widget.playerNumber}: ${widget.playerName}')));
-            },
-            child: Text('-'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              _addOwnGoal();
-              widget.notifyParent();
-              Scaffold.of(context).showSnackBar(SnackBar(
-                  content:
-                      Text('Own Goal for Player${widget.playerNumber}: ${widget.playerName}')));
-            },
-            child: Text('ET'),
-          ),
-        ],
-      )
-    ]);
+    return Container(
+        width: 160,
+        height: 160,
+        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(color: Colors.white.withOpacity(0.8),
+        borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 50,
+              top: 0,
+              width: 50,
+              height: 50,
+              child: FlatButton(
+                onPressed: () {
+                  _addGoalToPlayer();
+                  widget.notifyParent();
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                          'Goal! +1 for Player${widget.playerNumber}: ${widget.playerName}')));
+                },
+                shape: CircleBorder(),
+                color: Colors.blue,
+                child: Text(
+                  '+',
+                  style: TextStyle(fontSize: 28, color: Colors.white),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              top: 70,
+              width: 150,
+              child: Center(
+                child: Text('${widget.playerName}'),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              bottom: 0,
+              width: 50,
+              height: 50,
+              child: FlatButton(
+                onPressed: () {
+                  _removeGoalFromPlayer();
+                  widget.notifyParent();
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                          'Remove one Goal from Player${widget.playerNumber}: ${widget.playerName}')));
+                },
+                shape: CircleBorder(),
+                color: Colors.red,
+                child: Text(
+                  '-',
+                  style: TextStyle(fontSize: 28, color: Colors.white),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 0,
+              bottom: 0,
+              width: 50,
+              height: 50,
+              child: FlatButton(
+                onPressed: () {
+                  _addOwnGoal();
+                  widget.notifyParent();
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                          'Own Goal for Player${widget.playerNumber}: ${widget.playerName}')));
+                },
+                shape: CircleBorder(),
+                color: Colors.black,
+                child: Text('ET',
+                  style: TextStyle(fontSize: 12, color: Colors.white),),
+              ),
+            ),
+          ],
+        ));
   }
 }
